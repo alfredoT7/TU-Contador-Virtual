5eea42b88b8b022f85ba00d43f306cc43a7da4fb
alfredoT7 <123456alfredotorri@gmail.com>
2024-05-29 15:59:58 -0400

    Configs para el deploy4

--- build.sh
+++ build.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
 # Exit on error
 set -o errexit
-
 # Modify this line as needed for your package manager (pip, poetry, etc.)
-pip install -r requirements.txt
-#python populate_categories.py
+#pip install -r requirements.txt
 # Convert static asset files
 python manage.py collectstatic --no-input
-
 # Apply any outstanding database migrations
 python manage.py migrate
+python populate_categories.py
